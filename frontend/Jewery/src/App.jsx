2b8505ff6988d1460acf8960d2814fb5869d5ddd
Pawares <pawarest.putkung@gmail.com>
2025-10-30 14:44:00 +0700

    frontend update homepage

--- frontend/Jewery/src/App.jsx
+++ frontend/Jewery/src/App.jsx
@@ -1,39 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+<Router basename="/Jewery">
+      <Routes>
+        {/* Home page */}
+        <Route path="/" element={<Home />} />
 
-      <div className="h-screen flex items-center justify-center bg-blue-500 text-white text-4xl font-bold">
-      Tailwind is working!
-    </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        {/* Auth pages */}
+       
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
